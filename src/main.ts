fa408b706ebaa001e07b0c0dc3e604d76f0dfe13
Anton@habdevs <hramickih@yandex.ru>
2024-06-11 22:53:47 +0500

    chore: added sentry

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
+import '../sentry.init';
+import * as Sentry from '@sentry/node';
+import {
+  BaseExceptionFilter,
+  HttpAdapterHost,
+  NestFactory,
+} from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  Sentry.setupNestErrorHandler(app, new BaseExceptionFilter(httpAdapter));
   await app.listen(3000);
 }
 bootstrap();
