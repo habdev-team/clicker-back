8d507c0fe1e6d61e9155653d45d506193a1dd27c
Anton@habdevs <hramickih@yandex.ru>
2024-06-11 21:40:59 +0500

    chore: end test husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,8 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'error', // Запрещаем использование any
+    'no-console': 'error', // Запрещаем использование console.log
+    'prettier/prettier': 'error',
   },
 };
--- src/test.ts
+++
@@ -1,6 +0,0 @@
-// Использование any
-const testFunction = (arg: any) => {
-  console.log(arg); // Использование console.log
-};
-
-testFunction('Test3');
